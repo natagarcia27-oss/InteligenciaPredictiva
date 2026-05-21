2964e3d063ef892a640c2d34ef8ede6e334fefe0
natagarcia27-oss <natagarcia27@gmail.com>
2026-05-20 21:16:30 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,65 @@
- 
+import streamlit as st
+import pandas as pd
+
+# ==================================
+# CONFIGURACIÓN
+# ==================================
+
+st.set_page_config(
+    page_title="Inteligencia Territorial",
+    layout="wide"
+)
+
+# ==================================
+# TÍTULO
+# ==================================
+
+st.title(
+    "Sistema Inteligencia Predictiva Territorial"
+)
+
+# ==================================
+# CARGAR DATOS
+# ==================================
+
+@st.cache_data
+def cargar_datos():
+
+    return pd.read_csv(
+        "riesgo_municipal.csv"
+    )
+
+df = cargar_datos()
+
+# ==================================
+# KPIs
+# ==================================
+
+col1, col2, col3 = st.columns(3)
+
+col1.metric(
+    "Municipios",
+    df['MUNICIPIO'].nunique()
+)
+
+col2.metric(
+    "Eventos",
+    int(df['EVENTOS'].sum())
+)
+
+col3.metric(
+    "Registros",
+    len(df)
+)
+
+# ==================================
+# TABLA
+# ==================================
+
+st.subheader(
+    "Vista Estratégica"
+)
+
+st.dataframe(
+    df.head(100)
+)
